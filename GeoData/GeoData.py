9615eaa95ba53d413d0033199a83d35e518bbf58
John Swoboda <swoboj@bu.edu>
2014-09-02 16:03:28 -0400

    Added read method for custom h5 files

--- GeoData/GeoData.py
+++ GeoData/GeoData.py
@@ -12,11 +12,13 @@ import numpy as np
 import scipy as sp
 import tables
 
-VARNAMES = ['data','datanames','coordnames','sensorloc']
+VARNAMES = ['data','datanames','coordnames','dataloc','sensorloc','times']
 class GeoData(object):
+    '''This class will hold the information for geophysical data.'''
     # Variables
     # data - This is a dictionary with strings for keys only. The strings are the names of the
     def __init__(self,readmethod,inputs):
+        '''This will create an instance of the GeoData class by giving it a read method and the inputs in a tuple'''
         (self.data,self.datanames,self.coordnames,self.dataloc,self.sensorloc,self.times) = readmethod(inputs)
         
     def write_h5(self,filename):
@@ -37,7 +39,8 @@ class GeoData(object):
             h5file.createArray(group,cvar,vardict[cvar],'Static array')
         h5file.close()          
                     
-    # TODO might want to make this private method    
+# TODO might want to make this private method   
+# currently just give this to the init function and it will create a class instance.
 def read_h5(filename):
     h5file=tables.openFile(filename)
     output={}
@@ -46,9 +49,28 @@ def read_h5(filename):
         for array in h5file.listNodes(group, classname = 'Array'):
             output[group._v_pathname][array.name]=array.read()
     h5file.close()
-    for 
+    outarr = [pathparts(ipath) for ipath in output.keys()]
+    outlist = []
+    for ivar in VARNAMES:
+        firsttime = True
+        dictout = False
+        for npath,ipath in enumerate(outarr):
+            if ivar==ipath[0]:
+                if len(ipath)==1:
+                    outlist.append(output[output.keys()[npath]])
+                elif len(ipath)>1 and firsttime:
+                    firsttime = False
+                    dictout=True
+                    curdict = {ipath[1]:output[output.keys()[npath]]}
+                else:
+                    curdict[ipath[1]] = output[output.keys()[npath]]
+        if dictout:
+            outlist.append(dictout)
     
-  
+    return tuple(outlist)
+def readSRI_h5(filenames,timelims,):
+    '''This will read the SRI formated h5 files for RISR and PFISR'''
+    
 def pathparts(path):
     components = [] 
     while True:
