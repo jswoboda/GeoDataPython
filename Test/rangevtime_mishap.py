2800d249b8a686b18464f5918568d6741d43be88
michael <hirsch617@gmail.com>
2015-10-11 01:18:12 -0400

    handle no matching beam

--- Test/rangevtime_mishap.py
+++ Test/rangevtime_mishap.py
@@ -15,7 +15,7 @@ beamazel = [[-154.3, 77.5],
             [-149.69,78.56],
             [-159.5, 78.],
             [-154.3, 79.5],
-            [-154.3, 78.5]]
+            [-154.3, 278.5]]
 cmap = (None,None,None,'bwr')
 #titles=('$N_e$','$T_i$','$T_e$','$V_i$')
 titles=(None,)*4
@@ -27,12 +27,12 @@ def makeplot(isrName,tbounds,isrparams):
 
 #%% plot data
     #setup subplot to pass axes handles in to be filled with individual plots
-    fg,axs = subplots(4,4,sharex=True,sharey=True,figsize=(16,10))
+    fg,axs = subplots(5,4,sharex=True,sharey=True,figsize=(16,10))
 
     for j,(ae,axc) in enumerate(zip(beamazel,axs)):
         for i,(b,p,c,ax,tt) in enumerate(zip(vbnd,isrparams,cmap,axc,titles)):
             rangevstime(isr,ae,b,p[:2],tbounds=tbounds,title=tt,cmap=c,
-                        ax=ax,fig=fg,ic=i==0,ir=j==len(axs)-1)
+                        ax=ax,fig=fg,ic=i==0,ir=j==len(axs)-1,it=j==0)
 
 if __name__ == "__main__":
     tbounds=(parse('2011-03-01T10:15Z'),
