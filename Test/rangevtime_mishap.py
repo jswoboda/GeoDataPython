789cd62623c0d4dd9037100099ba078a867a1724
michael <hirsch617@gmail.com>
2015-10-12 04:09:15 -0400

    allow loading small part of large video file

--- Test/rangevtime_mishap.py
+++ Test/rangevtime_mishap.py
@@ -3,12 +3,17 @@
 load isr data vs time and altitude
 """
 from __future__ import division,absolute_import
-from matplotlib.pyplot import subplots, show,figure
+import logging
+from matplotlib.pyplot import subplots, show,figure,draw,pause
 from dateutil.parser import parse
+from pytz import UTC
+from datetime import datetime
 #
 from GeoData.plotting import rangevstime,plotbeamposGD
 #
 from load_isropt import load_pfisr_neo
+
+epoch = datetime(1970,1,1,tzinfo=UTC)
 
 vbnd = ((1e9,5e11),(500,2500),(500,2500),(-200,200))
 beamazel = [[-154.3, 77.5],
@@ -20,10 +25,15 @@ cmap = (None,None,None,'bwr')
 #titles=('$N_e$','$T_i$','$T_e$','$V_i$')
 titles=(None,)*4
 
-def makeplot(isrName,tbounds,isrparams):
+def makeplot(isrName,optName,azelfn,tbounds,isrparams):
+
+    treq = (datetime(2011,3,2,8,20,20,tzinfo=UTC),
+              datetime(2011,3,2,8,20,21,tzinfo=UTC))
+
+    treq = [(t-epoch).total_seconds() for t in treq]
 
     #load radar data into class
-    isr = load_pfisr_neo(isrName,isrparams=isrparams)[0]
+    isr,opt = load_pfisr_neo(isrName,optName,azelfn,isrparams=isrparams,treq=treq)
 
 #%% plot data
     #setup subplot to pass axes handles in to be filled with individual plots
@@ -33,8 +43,22 @@ def makeplot(isrName,tbounds,isrparams):
         for i,(b,p,c,ax,tt) in enumerate(zip(vbnd,isrparams,cmap,axc,titles)):
             rangevstime(isr,ae,b,p[:2],tbounds=tbounds,title=tt,cmap=c,
                         ax=ax,fig=fg,ic=i==0,ir=j==len(axs)-1,it=j==0)
-
+#%%
     plotbeamposGD(isr,minel=75.,elstep=5.)
+#%%
+    try:
+        fg = figure()
+        ax = fg.gca()
+        hi=ax.imshow(opt.data['optical'][0,...],vmin=50,vmax=250,
+                     interpolation='none',origin='lower')
+        fg.colorbar(hi,ax=ax)
+        ht = ax.set_title('')
+        for t,im in zip(opt.times[:,0],opt.data['optical']):
+            hi.set_data(im)
+            ht.set_text(datetime.fromtimestamp(t,tz=UTC))
+            draw(); pause(0.1)
+    except Exception as e:
+        logging.error('problem loading images  {}'.format(e))
 
 if __name__ == "__main__":
 #%%
@@ -43,7 +67,9 @@ if __name__ == "__main__":
     tbounds=(parse('2011-03-02T07:30Z'),
              parse('2011-03-02T09:00Z'))
 
-    makeplot('~/data/2011-03-02/pfa110302.002.hdf5',tbounds,isrparams)
+    makeplot('~/data/2011-03-02/pfa110302.002.hdf5',
+             '~/data/2011-03-02/110302_0819.h5',
+             '~/data/2011-03/calMishap2011Mar.h5',tbounds,isrparams)
 #%%
     if False:
         tbounds=(parse('2011-03-01T10:13Z'),
