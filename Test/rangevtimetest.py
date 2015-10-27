d8c8c7f01fc0a189969be30199be087b4e4ee1ca
Michael H <hirsch617@gmail.com>
2015-10-27 19:17:53 -0400

    correct demo typos

--- Test/rangevtimetest.py
+++ Test/rangevtimetest.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 """
-quad plot
-REQUIRES mayavi for upper left plot (python 2.7 only)
-
 @author: John Swoboda
 """
 from __future__ import division,absolute_import
@@ -15,13 +12,14 @@ from load_isropt import load_risromti
 def makeplot(risrName):
     risr = load_risromti(risrName)[0]
 #%%
-    ax = figure(figsize=(16, 10)).gca()
+    fg = figure(figsize=(16, 10))
+    ax = fg.gca()
 
     vbnd = [5e10,5e11]
     beampair = risr.dataloc[0,1:]
 
-    rangevstime(risr,beampair,vbnd,'ne',ax=ax)
+    rangevstime(risr,beampair,vbnd,'ne',ax=ax,fig=fg)
 
 if __name__ == "__main__":
-    makeplot('ran120219.004.hdf5')
+    makeplot('~/data/test/ran120219.004.hdf5')
     show()
