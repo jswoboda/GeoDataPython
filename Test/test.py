5c5119febc2a1f2ab4157895f5fe336adedc5807
michael <scienceopen@noreply.github.com>
2016-04-12 13:45:56 -0400

    correct path for selftest

--- Test/test.py
+++ Test/test.py
@@ -2,17 +2,21 @@
 """
 self-test for GeoDataPython
 """
+from os.path import dirname,join
 from numpy.testing import assert_allclose,run_module_suite
 #
 from load_isropt import load_risromti
 
+path=dirname(__file__)
+
 def test_risr():
-    isrfn = 'data/ran120219.004.hdf5'
-    omtifn = 'data/OMTIdata.h5'
+    isrfn = join(path,'data','ran120219.004.hdf5')
+    omtifn = join(path,'data','OMTIdata.h5')
     risr,omti = load_risromti(isrfn,omtifn)
     assert_allclose(risr.data['ne'][[36,136],[46,146]],
                     [119949930314.93805,79983425500.702927])
     assert_allclose(omti.data['optical'][[32,41],[22,39]],
                     [ 603.03568232,  611.20040632])
-#test_risr()
-run_module_suite()
+
+if __name__ == '__main__':
+    run_module_suite()
