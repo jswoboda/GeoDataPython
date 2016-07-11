843084699777a5821143f28e71820d5b0661ea86
scienceopen <scienceopen@users.noreply.github.com>
2016-07-11 18:00:02 -0400

    travis.yml refined

--- setup.py
+++ setup.py
@@ -13,12 +13,9 @@ from setuptools import setup
 import subprocess
 
 try:
-    subprocess.call(['conda','install','--yes','--file','requirements.txt'])
+    subprocess.call(['conda','install','--file','requirements.txt'])
 except Exception as e:
     print('tried conda in {}, but you will need to install packages in requirements.txt  {}'.format(exepath,e))
-
-with open('README.rst','r') as f:
-	long_description = f.read()
 
 setup(description='GeoData class and needed functions.',
       author='John Swoboda',
@@ -27,6 +24,5 @@ setup(description='GeoData class and needed functions.',
       extras_require={'mayavi':'mayavi'},
       packages=['GeoData'],
       name='GeoData',
-      long_description=long_description
 )
 
